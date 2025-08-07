c7b5c168202d19d5d4547ca2732ae10e5fb4f491
Itsanindita0 <banindita97@gmail.com>
2025-08-07 18:13:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express') //imported or required express
+const app = express()  //created the variable app using express
+const port = 4000
+
+app.get('/',(req, res) => {
+    res.send("hello world")
+})
+
+app.get('/twitter',(req, res) => {
+    res.send("anindita is sooooo pretttyyyy")
+})
+
+app.get('/login', (req, res) => {
+    res.send('<h1>please login at chai aur code<h1/>')
+})
+
+app.get('/youtube',(req, res) => {
+    res.send("<h2>hiii youtube user<h2/>")
+})
+
+
+app.listen(port, () => {
+    console.log(`example app listening on port ${port}`);
+    
+})
